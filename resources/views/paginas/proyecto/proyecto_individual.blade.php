f83ca64f79a66fed1f343a9c55a709aa83f61aa6
Syperhugeen <ventas@worldmaster.com.uy>
2016-10-19 21:08:38 -0200

    ok

---
+++ resources/views/paginas/proyecto/proyecto_individual.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.user_layout.user_layout')
+
+
+@section('title')
+ Proyecto Individual
+@stop
+
+@section('MetaContent')
+  Description Home 
+@stop
+
+@section('MetaRobot')
+ INDEX,FOLLOW
+@stop
+
+
+ 
+
+@section('content')
+
+ 
+
+ <!--para agregar los márgenes laterales-->
+ <div class="content-wrapper">
+
+
+    
+     
+
+
+ </div>
+
+@stop
---
+++ resources/views/paginas/proyecto/proyecto_listado.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.user_layout.user_layout')
+
+
+@section('title')
+ Listado de Proyectos
+@stop
+
+@section('MetaContent')
+  Description Home 
+@stop
+
+@section('MetaRobot')
+ INDEX,FOLLOW
+@stop
+
+
+ 
+
+@section('content')
+
+ 
+
+ <!--para agregar los márgenes laterales-->
+ <div class="content-wrapper">
+
+
+    
+     
+
+
+ </div>
+
+@stop
