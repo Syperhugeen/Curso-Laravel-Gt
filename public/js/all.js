8e2ff1645631b8afc18aefe11bf39220bf787466
Syperhugeen <ventas@worldmaster.com.uy>
2016-10-19 20:40:20 -0200

    ok

--- public/js/all.js
+++ public/js/all.js
@@ -8626,6 +8626,8 @@ $.fn.scrollTo = function( target, options, callback ){
 
 DispararForm('.admin-boton-editar',$('#form-admin-empresa-datos'));
 DispararForm('.admin-boton-editar',$('#form-admin-img-home-subir'));
+
+DispararForm('.boton-subir-img',$('#form-admin-subir-img-proyectos'));
 $('.bxslider').bxSlider();
  });
 
