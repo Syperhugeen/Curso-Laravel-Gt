fb6133b0a8ba57cef8a11eba357d66f2b8ac1993
Mauricio Costanzo <ventas@worldmaster.com.uy>
2016-10-26 17:01:37 -0300

    ok

--- resources/views/layouts/user_layout/header/partes/desplegado_user_auth.blade.php
+++ resources/views/layouts/user_layout/header/partes/desplegado_user_auth.blade.php
@@ -1,10 +1,12 @@
+ @if(!Auth::guest())
  <div class="contenido-auth-deplegado-navbar">              
    <ul>
 
       @if(Auth::user()->role != 'user')
       <li><a href="{{route('get_datos_home_web')}}">Admin</a></li>
       @endif
-      
+
       <li><a href="{{route('logout')}}">Salir</a></li>
    </ul>
 </div> 
+@endif
