27cea20916c3c342b4ebff81be901ae76a04f313
Syperhugeen <ventas@worldmaster.com.uy>
2016-10-28 01:08:06 -0300

    una tranza las putas tablas
    No toques nada por favor, es tremenda tranza. Además no se pueden usar
    clases de estilo para que funcione correctamente en todos lados. El
    header y el footer son parte de la tabla principal también.
    Por alguna razón no he podido darle estilo a algunos textos (ni al
    título que está a la derecha del header ni al texto del footer). Probé
    meter el estilo en la etiqueta td, en la tr, en la table, en la tbody,
    pero no funciona.

--- resources/views/emails/prueba.blade.php
+++ resources/views/emails/prueba.blade.php
@@ -1,5 +1,213 @@
 @extends('emails.layouts.layout_principal')
 
 @section('contenido')
- estos es contendido
+
+
+<table width="100%" cellspacing="0" cellpadding="0" border="0">
+ <tbody>
+  <tr>
+   <td bgcolor="#b3b3b3" valign="top" align="center">
+
+   <table width="600px" cellspacing="0" cellpadding="0" border="0">
+    <tbody>
+  	 <tr height="130px">
+  	  <td bgcolor="#3b3b3b" valign="top" align="left">
+
+	  	  <table width="100%" cellspacing="0" cellpadding="0" border="0">
+	    	<tbody>
+	  	 		<tr>
+	  	 			<td height="30px">
+	  	 			</td>
+	  	 		</tr>
+	  	 	</tbody>
+	  	  </table>
+
+	  	  <table width="100%" cellspacing="0" cellpadding="0" border="0">
+	    	<tbody>
+	  	 		<tr>
+	  	 			<td height="80px">
+
+						  	  <table width="100%" cellspacing="0" cellpadding="0" border="0">
+						    	<tbody>
+
+						  	 		<tr width="40px">
+						  	 			<td height="100%">
+						  	 			</td>
+						  	 		</tr>
+
+						  	 		<tr width="520px">
+						  	 			<td height="100%" valign="top" align="left">
+
+						  	 			<table width="100%" cellspacing="0" cellpadding="0" border="0">
+						  	 				<tbody>
+						  	 					<tr>
+
+													<td width="40px">
+													</td>
+
+						  	 						<td width="260px" valign="center" align="left">
+
+
+						  	 							<img src="http://localhost/Curso-Laravel-Gt/public/imagenes/Empresa/logo_cuadrado.png" height="60px">
+
+
+						  	 						</td>
+
+						  	 						<td width="260px" valign="center" align="right">
+
+						  	 							<table width="100%" cellspacing="0" cellpadding="0" border="0">
+						  	 								<tbody>
+
+						  	 									<tr height="40%"><td>
+						  	 									<table align="right" style="font-family:Verdana, Helvetica, Arial, sans-serif; font-color:#b3b3b3; font-size:14px"><tbody>
+						  	 									<tr>
+						  	 										<td align="right" style="font-family:Verdana, Helvetica, Arial, sans-serif; font-color:#b3b3b3; font-size:14px">
+						  	 										asunto:
+						  	 										</td>
+						  	 									</tr>
+						  	 									</tbody></table></td></tr>
+
+						  	 									<tr height="60%"><td>
+						  	 									<table align="right" style="font-family:Verdana, Helvetica, Arial, sans-serif; font-color:#b3b3b3; font-size:22px"><tbody>
+						  	 									<tr>
+						  	 										<td align="right" style="font-family:Verdana, Helvetica, Arial, sans-serif; font-color:#b3b3b3; font-size:22px">
+						  	 										CONTRASEÑA
+						  	 										</td>
+						  	 									</tr>
+						  	 									</tbody></table></td></tr>
+
+						  	 								</tbody>
+						  	 							</table>
+
+						  	 						</td>
+
+													<td width="40px">
+													</td>
+
+						  	 					</tr>
+						  	 				</tbody>
+						  	 			</table>
+
+						  	 			</td>
+						  	 		</tr>
+
+						  	 		<tr width="40px">
+						  	 			<td height="100%">
+						  	 			</td>
+						  	 		</tr>
+
+						  	 	</tbody>
+						  	  </table>
+
+	  	 			</td>
+	  	 		</tr>
+	  	 	</tbody>
+	  	  </table>
+
+	  	  <table width="100%" cellspacing="0" cellpadding="0" border="0">
+	    	<tbody>
+	  	 		<tr>
+	  	 			<td height="20px">
+	  	 			</td>
+	  	 		</tr>
+	  	 	</tbody>
+	  	  </table>
+
+  	  </td>
+  	 </tr>
+
+  	 <tr height="10px">
+  	 	<td width="100%" bgcolor="#4a9fff">
+  	 	</td>
+  	 </tr>
+
+  	 <tr valign="top" align="center">
+  	 	<td width="600px" bgcolor="#ffffff">
+  	 		<table>
+  	 			<tbody>
+  	 				<tr>
+
+  	 					<td width="40px">
+  	 					</td>
+
+
+  	 					<td width="520px">
+
+  	 						<table>
+  	 							<tbody>
+
+  	 								<tr height="40px"><td></td>
+  	 								</tr>
+
+   	 								<tr height="50px">
+  	 									<td width="100%" valign="top" align="left" style="font-family:Verdana, Helvetica, Arial, sans-serif; font-color:#858585; font-size:20px">
+  	 										Ernesto, restablece tu contraseña
+  	 									</td>
+  	 								</tr>
+
+  	 								<tr>
+  	 									<td width="100%" valign="top" align="left" style="font-family:Verdana, Helvetica, Arial, sans-serif; font-color:#b3b3b3; font-size:16px">
+  	 										Parece que has olvidado tu contraseña. Si es correcto, pulsa el siguiente botón o haz click en este link para que te enviemos una nueva:<br>http://todopadentro.com
+  	 									</td>
+  	 								</tr>
+  	 								<tr height="10px"><td></td>
+  	 								</tr>
+  	 								<tr height="30px" align="center"><td width="200px" style="font-family:Verdana, Helvetica, Arial, sans-serif; font-color:#FFFFFF; font-weight:600; font-size:22px;border-bottom-left-radius:5px;border-bottom-right-radius:5px;border-top-left-radius:5px;border-top-right-radius:5px" valign="center" align="center" bgcolor="#4a9fff">Restablecer</td>
+
+  	 								</tr>
+  	 								<tr height="20px"><td></td>
+  	 								</tr>
+  	 								<tr>
+  	 									<td width="100%" valign="top" align="left" style="font-family:Verdana, Helvetica, Arial, sans-serif; font-color:#b3b3b3; font-size:12px">
+  	 										Si has recibido este correo por error, no eres la persona a quien nos dirigimos o no has solicitado ayuda concerniente al olvido de tu contraseña; haznos el favor de comunicarte con nosotros.
+  	 									</td>
+  	 								</tr>
+  	 								<tr height="40px"><td></td>
+  	 								</tr>
+
+  	 							</tbody>
+  	 						</table>
+
+  	 					</td>
+
+  	 					<td width="40px">
+  	 					</td>
+
+  	 				</tr>
+  	 			</tbody>
+  	 		</table>
+  	 	</td>
+  	 </tr> 
+
+  	 <tr>
+  	 	<td>
+  	 		<table width="100%" height="80px">
+  	 			<tbody>
+  	 				<tr bgcolor="#3b3b3b">
+
+  	 					<td width="40px">
+  	 					</td>
+
+    	 				<td width="520px" valign="center" align="left" style="font-family:Verdana, Helvetica, Arial, sans-serif; font-color:#b3b3b3; font-size:8px">
+    	 				Acá van los legales al pie del mail, formas de contacto como número telefónico. También cosas como "no responda a este correo" o ese tipo de pajerías que harán de esta parte un párrafo de dos o tres renglones o tal vez más. Podemos poner "diseñado por etc", y poner links hacia nuestra web, y hacia la de nuestro cliente que envió el mail a su cliente automáticamente.
+  	 					</td>
+
+  	 					<td width="40px">
+  	 					</td>
+
+  	 				</tr>
+  	 			</tbody>
+  	 		</table>
+  	 	</td>
+  	 </tr> 	 
+
+  	</tbody>
+   </table>
+
+   </td>
+  </tr>
+ </tbody>
+</table>
+
+
 @stop
