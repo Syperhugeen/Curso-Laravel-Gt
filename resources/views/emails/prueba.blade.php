c765654a14d0d4cafc97998983d976810ee8316b
Syperhugeen <ventas@worldmaster.com.uy>
2016-10-28 01:38:22 -0300

    arreglado
    ok, arreglé el tema de los colores y un problema con la celda del botón
    que no admitía vínculo. Dejá todo así, no toques nada

--- resources/views/emails/prueba.blade.php
+++ resources/views/emails/prueba.blade.php
@@ -53,24 +53,24 @@
 
 						  	 						</td>
 
-						  	 						<td width="260px" valign="center" align="right">
+						  	 						<td width="250px" valign="center" align="right">
 
 						  	 							<table width="100%" cellspacing="0" cellpadding="0" border="0">
 						  	 								<tbody>
 
 						  	 									<tr height="40%"><td>
-						  	 									<table align="right" style="font-family:Verdana, Helvetica, Arial, sans-serif; font-color:#b3b3b3; font-size:14px"><tbody>
+						  	 									<table align="right" style="font-family:Verdana, Helvetica, Arial, sans-serif; color:#b3b3b3; font-size:14px"><tbody>
 						  	 									<tr>
-						  	 										<td align="right" style="font-family:Verdana, Helvetica, Arial, sans-serif; font-color:#b3b3b3; font-size:14px">
+						  	 										<td align="right" style="font-family:Verdana, Helvetica, Arial, sans-serif; color:#b3b3b3; font-size:14px">
 						  	 										asunto:
 						  	 										</td>
 						  	 									</tr>
 						  	 									</tbody></table></td></tr>
 
 						  	 									<tr height="60%"><td>
-						  	 									<table align="right" style="font-family:Verdana, Helvetica, Arial, sans-serif; font-color:#b3b3b3; font-size:22px"><tbody>
+						  	 									<table align="right" style="font-family:Verdana, Helvetica, Arial, sans-serif; color:#b3b3b3; font-size:22px"><tbody>
 						  	 									<tr>
-						  	 										<td align="right" style="font-family:Verdana, Helvetica, Arial, sans-serif; font-color:#b3b3b3; font-size:22px">
+						  	 										<td align="right" style="font-family:Verdana, Helvetica, Arial, sans-serif; color:#b3b3b3; font-size:22px">
 						  	 										CONTRASEÑA
 						  	 										</td>
 						  	 									</tr>
@@ -80,6 +80,12 @@
 						  	 							</table>
 
 						  	 						</td>
+
+                            <td width="7px" valign="center" align="right">
+                            </td>
+
+                            <td width="3px" valign="center" align="right" bgcolor="#b3b3b3">
+                            </td>
 
 													<td width="40px">
 													</td>
@@ -140,28 +146,48 @@
   	 								</tr>
 
    	 								<tr height="50px">
-  	 									<td width="100%" valign="top" align="left" style="font-family:Verdana, Helvetica, Arial, sans-serif; font-color:#858585; font-size:20px">
+  	 									<td width="100%" valign="top" align="left" style="font-family:Verdana, Helvetica, Arial, sans-serif; color:#858585; font-size:20px">
   	 										Ernesto, restablece tu contraseña
   	 									</td>
   	 								</tr>
 
   	 								<tr>
-  	 									<td width="100%" valign="top" align="left" style="font-family:Verdana, Helvetica, Arial, sans-serif; font-color:#b3b3b3; font-size:16px">
-  	 										Parece que has olvidado tu contraseña. Si es correcto, pulsa el siguiente botón o haz click en este link para que te enviemos una nueva:<br>http://todopadentro.com
+  	 									<td width="100%" valign="top" align="left" style="font-family:Verdana, Helvetica, Arial, sans-serif; color:#b3b3b3; font-size:16px">
+  	 										Parece que has olvidado tu contraseña. Si es correcto, pulsa el siguiente botón o haz click en este link para que te enviemos una nueva:<br><a href="http://todopadentro.com"> http://todopadentro.com</a>
   	 									</td>
   	 								</tr>
-  	 								<tr height="10px"><td></td>
+  	 								<tr height="40px"><td></td>
   	 								</tr>
-  	 								<tr height="30px" align="center"><td width="200px" style="font-family:Verdana, Helvetica, Arial, sans-serif; font-color:#FFFFFF; font-weight:600; font-size:22px;border-bottom-left-radius:5px;border-bottom-right-radius:5px;border-top-left-radius:5px;border-top-right-radius:5px" valign="center" align="center" bgcolor="#4a9fff">Restablecer</td>
 
+                    <tr><td>
+                            <table><tbody>
+                            <tr height="40px" align="center">
+
+                              <td width="155px"></td>
+
+            	 								<td width="210px" style="font-family:Verdana, Helvetica, Arial, sans-serif; color:#FFFFFF; font-size:22px;border-bottom-left-radius:5px;border-bottom-right-radius:5px;border-top-left-radius:5px;border-top-right-radius:5px" valign="center" align="center" bgcolor="#4a9fff">
+                                <a href="http://todopadentro.com" style="color:#FFFFFF;">
+                                  <div width="100%" height="100%">
+                                    Restablecer
+                                  </div>
+                                </a>
+                              </td>
+
+                              <td width="155px"></td>
+
+                            </tr>
+                            </tbody></table>
+  	 								</td></tr>
+
+  	 								<tr height="60px"><td></td>
   	 								</tr>
-  	 								<tr height="20px"><td></td>
-  	 								</tr>
+
   	 								<tr>
-  	 									<td width="100%" valign="top" align="left" style="font-family:Verdana, Helvetica, Arial, sans-serif; font-color:#b3b3b3; font-size:12px">
+  	 									<td width="100%" valign="top" align="left" style="font-family:Verdana, Helvetica, Arial, sans-serif; color:#b3b3b3; font-size:12px">
   	 										Si has recibido este correo por error, no eres la persona a quien nos dirigimos o no has solicitado ayuda concerniente al olvido de tu contraseña; haznos el favor de comunicarte con nosotros.
   	 									</td>
   	 								</tr>
+
   	 								<tr height="40px"><td></td>
   	 								</tr>
 
@@ -181,14 +207,14 @@
 
   	 <tr>
   	 	<td>
-  	 		<table width="100%" height="80px">
-  	 			<tbody>
-  	 				<tr bgcolor="#3b3b3b">
+  	 		<table width="100%" height="80px" style="font-family:Verdana, Helvetica, Arial, sans-serif; color:#b3b3b3; font-size:8px">
+  	 			<tbody style="font-family:Verdana, Helvetica, Arial, sans-serif; color:#b3b3b3; font-size:8px">
+  	 				<tr bgcolor="#3b3b3b" style="font-family:Verdana, Helvetica, Arial, sans-serif; color:#b3b3b3; font-size:8px">
 
   	 					<td width="40px">
   	 					</td>
 
-    	 				<td width="520px" valign="center" align="left" style="font-family:Verdana, Helvetica, Arial, sans-serif; font-color:#b3b3b3; font-size:8px">
+    	 				<td width="520px" valign="center" align="left" style="font-family:Verdana, Helvetica, Arial, sans-serif; color:#b3b3b3; font-size:9px">
     	 				Acá van los legales al pie del mail, formas de contacto como número telefónico. También cosas como "no responda a este correo" o ese tipo de pajerías que harán de esta parte un párrafo de dos o tres renglones o tal vez más. Podemos poner "diseñado por etc", y poner links hacia nuestra web, y hacia la de nuestro cliente que envió el mail a su cliente automáticamente.
   	 					</td>
 
