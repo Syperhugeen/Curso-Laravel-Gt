c78414a87a0474a65620d893cf48694d1266bfd8
Mauricio Costanzo <ventas@worldmaster.com.uy>
2016-11-02 15:19:54 -0300

    ok

--- app/Http/Rutas/Publicas.php
+++ app/Http/Rutas/Publicas.php
@@ -2,6 +2,11 @@
 
 
 
+require __DIR__ . '/Noticias/Rutas_Noticias_Publicas.php';
+
+require __DIR__ . '/Proyectos/Rutas_Proyectos_Publicas.php';
+
+
 
 
 //Ruta de Home
@@ -29,28 +34,7 @@ Route::get('/Servicios' , [
   'as'   => 'get_pagina_servicios']
 );
 
-//Noticias
-Route::get('/Noticias' , [                    
-  'uses' => 'Publicas\Paginas_Controller@get_pagina_noticias_listado',
-  'as'   => 'get_pagina_noticias_listado']
-);
-     //Noticia Individual
-      Route::get('/Publicacion/{name}/{id}' , [                    
-        'uses' => 'Publicas\Paginas_Controller@get_pagina_noticia_individual',
-        'as'   => 'get_pagina_noticia_individual']
-      );
 
 
- //Proyectos
-Route::get('/Proyecto/{name}/{id}' , [                    
-  'uses' => 'Publicas\Paginas_Controller@get_pagina_proyecto_individual',
-  'as'   => 'get_pagina_proyecto_individual']
-);
 
-
-//Noticias Individuales
-Route::get('/Proyectos' , [                    
-  'uses' => 'Publicas\Paginas_Controller@get_pagina_proyecto_listado',
-  'as'   => 'get_pagina_proyecto_listado']
-);
 
