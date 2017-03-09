c0925a0f2f54a3c1bbae7f57cd901b581ceae3f3
Eruni えるねすと <eccb.contacto@gmail.com>
2017-03-09 19:25:42 -0300

    asdf
    asdf

--- resources/views/paginas/eventos/entidad_evento_para_listado.blade.php
+++ resources/views/paginas/eventos/entidad_evento_para_listado.blade.php
@@ -1,23 +1,21 @@
-		
-		<div class="col-md-6 special-col no-padding">
+	<div class="col-xs-12 special-col">
+	<div class="row">
+
+		<div class="col-md-6 no-padding">
 			<div class="event-col"></div>
 			<img class="img-greyscale" src="{{$Evento->url_img}}">
 		</div>
 
-
-
-
-		<div class="col-md-6 special-col table-outer">
-
+		<div class="col-md-6 table-outer">
 			{{-- contiene el último evento --}}
 			<div class="table-inner">
 				<h2>{{$Evento->name}}</h2>
-				<p>{{$Evento->description}}</p>
+				<p class="text-center">{{$Evento->description}}</p>
 				<a href="{{$Evento->route}}">
 					<h5 class="ampliar"><span class="glyphicon glyphicon-chevron-right"></span>AMPLIAR</h5>
 				</a>
 			</div>
-		
 		</div>
 
-		</div>
+	</div>
+	</div>
