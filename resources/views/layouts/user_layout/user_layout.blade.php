266793176d918c43b1b9180ad0937575b88be84b
Eruni <eccb.contacto@gmail.com>
2017-12-23 06:05:21 -0300

    Favicons
    Agregué meta-tags en el head del layout para que muestre los iconos
    según el dispositivo y el tamaño de icono. También agregué un meta-tag
    para cuando se comparte el link de la web por facebook por ejemplo, para
    que aparezca una imagen en particular.
    
    Todas esas imágenes e iconos tienen que estar en el root de la web

--- resources/views/layouts/user_layout/user_layout.blade.php
+++ resources/views/layouts/user_layout/user_layout.blade.php
@@ -5,6 +5,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- social media meta-tags for proper sharing
+    (la imagen tiene que estar en la carpeta root, ratio 1.91:1 1200x630px) -->
+    <meta property="og:title" content="Global Target">
+    <meta property="og:description" content="Agencia de modelos, eventos y promociones.">
+    <meta property="og:image" content="http://www.globaltarget.com.uy/thumbnail.jpg">
+    <meta property="og:url" content="http://www.globaltarget.com.uy/">
+    
+    <!-- Favicons (tienen que estar en la carpeta root) -->
+    <link rel="icon" type="image/png" href="http://www.globaltarget.com.uy/favicon-16x16.png" sizes="16x16">  
+    <link rel="icon" type="image/png" href="http://www.globaltarget.com.uy/favicon-32x32.png" sizes="32x32">  
+    <link rel="icon" type="image/png" href="http://www.globaltarget.com.uy/favicon-96x96.png" sizes="96x96">
+
+    <!-- Apple Touch Icons (tienen que estar en la carpeta root) -->
+    <link rel="apple-touch-icon" href="older-iPhone.png"> <!-- // 120px -->  
+    <link rel="apple-touch-icon" sizes="180x180" href="iPhone-6-Plus.png">  
+    <link rel="apple-touch-icon" sizes="152x152" href="iPad-Retina.png">  
+    <link rel="apple-touch-icon" sizes="167x167" href="iPad-Pro.png">
+
     <title>{{$Empresa->name}} - @yield('title')</title>
     <meta name="Description" CONTENT="@yield('MetaContent')">
 
