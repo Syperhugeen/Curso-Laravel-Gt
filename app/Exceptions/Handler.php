c597f4f0a3279564338fdb73cc879f9e764e8e47
Mauricio Costanzo <ventas@worldmaster.com.uy>
2018-02-28 09:28:15 -0300

    sd

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -39,13 +39,15 @@ class Handler extends ExceptionHandler
     public function render($request, Exception $e)
     {
 
-        
-        if ($e->getStatusCode() == 500) {
-            return view('errors.500');
+
+        // 404 page when a model is not found
+        if ($e instanceof ModelNotFoundException) {
+            return response()->view('errors.404', [], 404);
         }
 
-        if ($e->getStatusCode() == 404) {
-            return view('errors.404');
+        // Custom error 500 view on production
+        if (app()->environment() == 'production') {
+            return response()->view('errors.500', [], 500);
         }
 
         return parent::render($request, $e);
