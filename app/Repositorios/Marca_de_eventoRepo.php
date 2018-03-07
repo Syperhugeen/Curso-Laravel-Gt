ef31d9091031c28415e05351f426c1c1e7d2e691
Mauricio Costanzo <ventas@worldmaster.com.uy>
2018-03-07 20:03:05 -0300

    sd

--- app/Repositorios/Marca_de_eventoRepo.php
+++ app/Repositorios/Marca_de_eventoRepo.php
@@ -45,7 +45,7 @@ class Marca_de_eventoRepo extends BaseRepo
         //filtro las que no estan activas  
         $coleccion = $coleccion->filter(function ($value) {
 
-         $value->evento->estado = 'si';
+         $value->evento->estado = 'no';
 
         });                    
       
