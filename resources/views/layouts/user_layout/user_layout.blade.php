63b1bc821f60e205971ceea1ffff09e3635ac9c9
Eruni <eccb.contacto@gmail.com>
2018-10-05 12:25:10 -0300

    correccion viernes
    correccion viernes

--- resources/views/layouts/user_layout/user_layout.blade.php
+++ resources/views/layouts/user_layout/user_layout.blade.php
@@ -5,44 +5,45 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- social media meta-tags for proper sharing -->
-    (la imagen tiene que estar en la carpeta root, ratio 1.91:1 1200x630px) -->
-    <meta property="og:title" content="Global Target">
-    <meta property="og:description" content="Agencia de modelos, eventos y promociones.">
-    <meta property="og:image" content="https://www.globaltarget.com.uy/thumbnail.jpg">
-    <meta property="og:url" content="https://www.globaltarget.com.uy/">
-    
-    <!-- Favicons (tienen que estar en la carpeta root) -->
-    <link rel="icon" type="image/png" href="https://www.globaltarget.com.uy/favicon-16x16.png" sizes="16x16">  
-    <link rel="icon" type="image/png" href="https://www.globaltarget.com.uy/favicon-32x32.png" sizes="32x32">  
-    <link rel="icon" type="image/png" href="https://www.globaltarget.com.uy/favicon-96x96.png" sizes="96x96">
+<!-- META-TAGS DE FAVICONS, DISPOSITIVOS Y THUMBNAILS -->
+      <!-- social media meta-tags for proper sharing
+      (la imagen tiene que estar en la carpeta root, ratio 1.91:1 1200x630px) -->
+      <meta property="og:title" content="Global Target">
+      <meta property="og:description" content="Agencia de modelos, eventos y promociones.">
+      <meta property="og:image" content="https://www.globaltarget.com.uy/thumbnail.jpg">
+      <meta property="og:url" content="https://www.globaltarget.com.uy/">
+      
+      <!-- Favicons (tienen que estar en la carpeta root) -->
+      <link rel="icon" type="image/png" href="https://www.globaltarget.com.uy/favicon-16x16.png" sizes="16x16">  
+      <link rel="icon" type="image/png" href="https://www.globaltarget.com.uy/favicon-32x32.png" sizes="32x32">  
+      <link rel="icon" type="image/png" href="https://www.globaltarget.com.uy/favicon-96x96.png" sizes="96x96">
 
-    <!-- Apple Touch Icons (tienen que estar en la carpeta root) -->
-    <link rel="apple-touch-icon" href="older-iPhone.png"> <!-- // 120px -->  
-    <link rel="apple-touch-icon" sizes="180x180" href="iPhone-6-Plus.png">  
-    <link rel="apple-touch-icon" sizes="152x152" href="iPad-Retina.png">  
-    <link rel="apple-touch-icon" sizes="167x167" href="iPad-Pro.png">
+      <!-- Apple Touch Icons (tienen que estar en la carpeta root) -->
+      <link rel="apple-touch-icon" href="older-iPhone.png"> <!-- // 120px -->  
+      <link rel="apple-touch-icon" sizes="180x180" href="iPhone-6-Plus.png">  
+      <link rel="apple-touch-icon" sizes="152x152" href="iPad-Retina.png">  
+      <link rel="apple-touch-icon" sizes="167x167" href="iPad-Pro.png">
 
-    <title>{{$Empresa->name}} - @yield('title')</title>
-    <meta name="Description" CONTENT="@yield('MetaContent')">
+      <title>{{$Empresa->name}} - @yield('title')</title>
+      <meta name="Description" CONTENT="@yield('MetaContent')">
 
-    <link rel="stylesheet" type="text/css" href="{{url()}}{{ elixir('css/app.css') }}">   
-    <link rel="stylesheet" type="text/css" href=" {{ asset('Iconos/fonts/style.css')}}">
-    <META name="robots" content="@yield('MetaRobot')">
+      <link rel="stylesheet" type="text/css" href="{{url()}}{{ elixir('css/app.css') }}">   
+      <link rel="stylesheet" type="text/css" href=" {{ asset('Iconos/fonts/style.css')}}">
+      <META name="robots" content="@yield('MetaRobot')">
 
-<!-- realfavicongenerator headlines -->
-<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=eEGWoRWgP3">
-<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=eEGWoRWgP3">
-<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=eEGWoRWgP3">
-<link rel="manifest" href="/site.webmanifest?v=eEGWoRWgP3">
-<link rel="mask-icon" href="/safari-pinned-tab.svg?v=eEGWoRWgP3" color="#caa752">
-<link rel="shortcut icon" href="/favicon.ico?v=eEGWoRWgP3">
-<meta name="apple-mobile-web-app-title" content="GlobalTarget">
-<meta name="application-name" content="GlobalTarget">
-<meta name="msapplication-TileColor" content="#caa752">
-<meta name="theme-color" content="#ffffff">
-<!-- / fin de realfavicongenerator headlines -->
-
+    <!-- realfavicongenerator headlines -->
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=eEGWoRWgP3">
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=eEGWoRWgP3">
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=eEGWoRWgP3">
+      <link rel="manifest" href="/site.webmanifest?v=eEGWoRWgP3">
+      <link rel="mask-icon" href="/safari-pinned-tab.svg?v=eEGWoRWgP3" color="#caa752">
+      <link rel="shortcut icon" href="/favicon.ico?v=eEGWoRWgP3">
+      <meta name="apple-mobile-web-app-title" content="GlobalTarget">
+      <meta name="application-name" content="GlobalTarget">
+      <meta name="msapplication-TileColor" content="#caa752">
+      <meta name="theme-color" content="#ffffff">
+   <!-- / fin de realfavicongenerator headlines -->
+<!-- / fin de META-TAGS DE FAVICONS, DISPOSITIVOS Y THUMBNAILS -->
 
 <!-- Global site tag (gtag.js) - Google Ads: 794875575 -->
 
