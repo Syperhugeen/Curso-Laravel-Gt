be092f109453053c6b00aaa2c5596ae03f2944a9
Mauricio Costanzo <ventas@worldmaster.com.uy>
2018-11-27 18:30:54 -0300

    df

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,15 +2,15 @@
 
 
 @section('title')
- Registro de Usuario en Worldmaster Uruguay 
+ Registro de usuario
 @stop
 
 @section('MetaContent')
-  Crea tu Cuenta en Worldmaster y accede al sitio #1 en atención de Uruguay.  
+  
 @stop
 
 @section('MetaRobot')
- INDEX,FOLLOW
+ NoINDEX,NoFOLLOW
 @stop
 
 
@@ -36,7 +36,7 @@
     <div class="container-fluid section-wrapper">
      <div class="row">    
       <div class="col-sm-8 col-sm-push-2 col-md-6 col-md-push-3 col-lg-4 col-lg-push-4 wow fadeInUp">
-       @include('formularios.auth.register_form')
+        @include('formularios.auth.register_form')
       </div>
     </div>
   </div>
