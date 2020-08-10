32cb65cb1951a6923ab9233da8cfa9dbf2654934
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-08-10 11:40:55 -0300

    fg

--- resources/views/layouts/user_layout/footer/footer.blade.php
+++ resources/views/layouts/user_layout/footer/footer.blade.php
@@ -5,8 +5,71 @@
 
 
 
-
+   @if(!Auth::guest())
+      @if(Auth::user()->first_name == 'Mauricio')
    <!-- primer row (el de los links de contacto y síguenos)-->
+   <div class="container-fluid">
+   <div class="row">
+
+    <!-- columna del isologo -->
+    <div class="col-md-3 footer-line hidden-xs hidden-sm footer-col-height" style="text-align:right;">
+      <div class="footer-isologo-container footer-col-height">
+        <img class="img-responsive footer-isologo" src="imagenes/Empresa/isologo_golden_bright_big.png">
+      </div>
+    </div>
+
+    {{-- Logos nuevos --}}
+     <div class="col-md-3 footer-line hidden-xs hidden-sm footer-col-height">
+      <div class="footer-isologo-container footer-col-height">
+        <img class="img-responsive footer-isologo" src="imagenes/Premios/awards-2019.png">
+      </div>
+    </div>
+
+    <!-- columna de datos de contácto -->
+    <div class="col-md-3 footer-line footer-responsive-vertical-space footer-col-height">
+
+      <h5 class="footer-title">contáctanos</h5>
+
+     @if($Empresa->telefono != null) 
+      <h5><span class="glyphicon glyphicon-earphone"></span> {{$Empresa->telefono}}</h5>
+    @endif
+
+        @if($Empresa->celular != null)
+      <h5><span class="glyphicon glyphicon-phone""></span> {{$Empresa->celular}}</h5>
+     @endif 
+
+     @if($Empresa->email != null)
+      <h5><span class="glyphicon glyphicon-envelope"></span> {{$Empresa->email}}</h5>
+     @endif
+
+     
+     @if($Empresa->direccion != null) 
+      <h5><span class="glyphicon glyphicon-map-marker"></span> {{$Empresa->direccion}}</h5>
+     @endif
+
+     @if($Empresa->horarios_dias != null) 
+      <h5><span class="glyphicon glyphicon-time"></span> {{$Empresa->horarios_dias}}</h5>
+     @endif
+
+    </div>
+
+  
+    <!-- columna de datos de redes sociales y mailing letters -->
+    <div class="col-md-3 footer-line3 footer-responsive-vertical-space footer-col-height">
+
+      <h5 class="footer-title">síguenos</h5>
+
+      <a href="https://www.facebook.com/GLOBALTARGET/" target="_blank" alt="Global Target social media"><h5><span class="socicon socicon-facebook"></span> /globaltarget</h5></a>
+      <a href="https://www.instagram.com/agencia_globaltarget/" target="_blank" alt="Global Target social media"><h5><span class="socicon socicon-instagram"></span> /agencia_globaltarget</h5> </a>
+      <a href="https://www.linkedin.com/company/agencia-global-target" target="_blank" alt="Global Target social media"><h5><span class="socicon socicon-linkedin"></span> /agencia-global-target</h5></a>
+    </div>
+
+
+  </div>
+ </div>
+ @endif
+ @else
+    <!-- primer row (el de los links de contacto y síguenos)-->
    <div class="container-fluid">
    <div class="row">
 
@@ -16,6 +79,8 @@
         <img class="img-responsive footer-isologo" src="imagenes/Empresa/isologo_golden_bright_big.png">
       </div>
     </div>
+
+    {{-- Logos nuevos --}}
 
 
     <!-- columna de datos de contácto -->
@@ -59,7 +124,8 @@
 
 
   </div>
- </div>
+
+ @endif
 
 
 
