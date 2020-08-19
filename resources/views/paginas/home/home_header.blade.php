40eea83b4600f109762195517f483d62b2afa469
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-08-19 11:22:56 -0300

    df

--- resources/views/paginas/home/home_header.blade.php
+++ resources/views/paginas/home/home_header.blade.php
@@ -5,6 +5,13 @@
   <!--<div><img class="img-responsive index-1" src="imagenes/Img/bg-home-header-newest.jpg"></div>-->
   <!--<img src="imagenes/Img/header_15years(responsive).jpg" alt="" class="img-responsive visible-xs">-->
   <div class="premio-contenedor">
+
+
+    @if(!Auth::guest())
+     @if(Auth::user()->first_name != 'Mauricio')
+       <a href="https://www.globaltarget.com.uy/evento-Latin_American_Quality_Awards_2018-419" class="Helper-OrdenarHijos-Row Helper-OrdenarHijos-Row-space-around premio-contenedor-img"><img src="imagenes/Img/premio_latinoamericano-2.png" class="img-responsive " style="bottom: 0px;"></a>
+      @endif 
+    @endif
     <a href="https://www.globaltarget.com.uy/evento-Latin_American_Quality_Awards_2018-419" class="Helper-OrdenarHijos-Row Helper-OrdenarHijos-Row-space-around premio-contenedor-img"><img src="imagenes/Img/premio_latinoamericano.png" class="img-responsive " style="bottom: 0px;"></a>
     <!--<div class="home-header-frase super-space-top">
       <a href="https://www.globaltarget.com.uy/evento-Latin_American_Quality_Awards_2018-419"><h1>PREMIO LATINOAMERICANO<br>POR CALIDAD TOTAL Y EXCELENCIA</h1></a>
